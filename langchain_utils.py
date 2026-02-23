53bb2c2e593e776d97d58fd4348c7a3491f4abba
prince2404 <166131044+prince2404@users.noreply.github.com>
2026-02-23 11:47:54 +0530

    update version

--- langchain_utils.py
+++ langchain_utils.py
@@ -1,7 +1,8 @@
 from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.history_aware_retriever import create_history_aware_retriever
+from langchain.chains.retrieval import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from typing import List
 from langchain_core.documents import Document
